773771338e249fa1cb41a9532ab06ae6a9d67d2c
Cristian Campos Agüero <ccampos04@gmail.com>
2026-03-11 08:58:33 -0600

    Update testing.py

--- testing.py
+++ testing.py
@@ -3,22 +3,6 @@
 import Laboratorio01;
 import pytest;
 
-    
-def test_lab_1():
-    assert Laboratorio01.calcularRenta(700000) == 0
-    
-def test_lab_2():
-    assert Laboratorio01.calcularRenta(1000000) == 18300
-    
-def test_lab_3():
-    assert Laboratorio01.calcularRenta(2000000) == 157000
-
-def test_lab_4():
-    assert isinstance(Laboratorio01.calcularRenta("2000000"), str) == isinstance("Error: El valor debe ser de tipo entero", str)
-
-def test_lab_5():
-    assert isinstance(Laboratorio01.calcularRenta(-2000000), str) == isinstance("Error: El valor debe ser mayor a CERO", str)
-
 ##################################################################
     
 def test_lab_6():
@@ -52,3 +36,33 @@ def test_lab_14():
 
 def test_lab_15():
     assert isinstance(Laboratorio01.calculadora (14, 4, 8), str) == isinstance("Error: El parámetro operacion debe ser un valor menor a 5", str)
+
+##################################################################
+
+def test_lab_16():
+    assert Laboratorio01.sumatoria_V2(1, 5, 1, 0)  == 15
+    
+def test_lab_17():
+    assert Laboratorio01.sumatoria_V2(1, 5, 1, 2)  == 9
+    
+def test_lab_18():
+    assert Laboratorio01.sumatoria_V2(1, 5, 3, 2)  == 1
+    
+def test_lab_19():
+    assert Laboratorio01.sumatoria_V2(10, 8, -1, 0)  == 27
+    
+def test_lab_20():
+    assert isinstance(Laboratorio01.sumatoria_V2(10, 5, 3, 2), str)  == 
+isinstance("Error: el parámetro inicio debe ser menor o igual a fin", str)
+
+def test_lab_21():
+    assert isinstance(Laboratorio01.sumatoria_V2(10, 15, -3, 2), str)  == 
+isinstance("Error: El parámetro de fin debe ser menor o igual a inicio", str)
+
+def test_lab_22():
+    assert isinstance(Laboratorio01.sumatoria_V2("10", 15, -3, 2), str)  == 
+isinstance("Error: Los parámetros deben ser enteros", str)
+
+def test_lab_23():
+    assert isinstance(Laboratorio01.sumatoria_V2(10, 15, 10, 2), str)  == 
+isinstance("Error: El parámetro distancia debe estar en 1 a 9", str)
